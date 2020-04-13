df4e52471f031ef53afcdfd5658fe924c1515500
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-04-13 21:09:02 +0200

    Added Scraper and sample API demo

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
-
+/*
+* @author: NicoDs96
+* @description: this code is a simple express API demo.
+* */
 var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+  res.send('Hello World!');
+});
+
+
+//THIS MUST BE THE LAST ROUTE
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
 });
+
+
