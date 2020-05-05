192c0dde5f3a05001b14ef7496b71030c954402a
uoogio <giovanni.buono12@gmail.com>
2020-05-05 16:07:05 +0200

    Android studio project

---
+++ AndroidProj/app/src/main/java/com/example/login/SessionManagment.java
@@ -0,0 +1,46 @@
+package com.example.login;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SessionManagment {
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
+    String SHARED_PREF_NAME = "session";
+    String SESSION_KEY = "session_user";
+
+
+            public SessionManagment(Context context) {
+                sharedPreferences = context.getSharedPreferences(SHARED_PREF_NAME, Context.MODE_PRIVATE);
+                editor = sharedPreferences.edit();
+
+            }
+
+            public void saveSession(User user){
+
+                // save session of user when is logged
+               String name = user.getName();
+               String email = user.getEmail();
+
+               editor.putString(SESSION_KEY,email).commit();
+
+                }
+
+
+             public String getSession(){
+
+               // return user whose session is saved
+
+              return sharedPreferences.getString(SESSION_KEY,"none");
+
+                }
+
+
+
+
+             public  void    removeSession(){
+
+                editor.putString(SESSION_KEY,"none").commit();
+
+             }
+}
