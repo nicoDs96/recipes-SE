5db4d6fc4aba2732d22e0bc8df2c9e3c4119a2bc
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-05-21 11:13:06 +0200

    fixed hint showing as text bug

--- AndroidProj/app/src/main/java/com/example/recipeSE/search/utils/SearchResultsAdapter.java
+++ AndroidProj/app/src/main/java/com/example/recipeSE/search/utils/SearchResultsAdapter.java
@@ -11,7 +11,6 @@ import android.widget.TextView;
 import com.example.recipeSE.R;
 import com.google.android.material.card.MaterialCardView;
 
-import org.w3c.dom.Text;
 
 import java.util.List;
 import java.util.Map;
