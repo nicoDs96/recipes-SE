981dabf8df718f3f8735a45ac0e7a286144f3c93
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-05-21 11:08:36 +0200

    added working cardview to show recipes

--- AndroidProj/app/src/main/java/com/example/recipeSE/search/utils/SearchResultsAdapter.java
+++ AndroidProj/app/src/main/java/com/example/recipeSE/search/utils/SearchResultsAdapter.java
@@ -1,14 +1,23 @@
 package com.example.recipeSE.search.utils;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.recipeSE.R;
+import com.google.android.material.card.MaterialCardView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
+import java.util.Map;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdapter.RecipeViewHolder> {
@@ -21,10 +30,20 @@ public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdap
     //TODO: replace textview vith cards with cards
     public static class RecipeViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView textView;
-        public RecipeViewHolder(TextView v) {
-            super(v);
-            textView = v;
+        public TextView title;
+        public TextView cal;
+        public TextView ingredients;
+        public TextView quantities;
+        public Button href;
+
+        public RecipeViewHolder(@NonNull View itemView, TextView title, TextView cal,
+                                TextView ingredients, TextView quantities, Button href) {
+            super(itemView);
+            this.title = title;
+            this.cal = cal;
+            this.ingredients = ingredients;
+            this.quantities = quantities;
+            this.href = href;
         }
     }
 
@@ -38,10 +57,18 @@ public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdap
     public RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // create a new view
         // TODO: replace textview vith cards with cards
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        MaterialCardView v = (MaterialCardView) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.view_search_text, parent, false);
 
-        RecipeViewHolder vh = new RecipeViewHolder(v);
+
+
+        RecipeViewHolder vh = new RecipeViewHolder(v,
+                (TextView) v.findViewById(R.id.Title),
+                (TextView) v.findViewById(R.id.cal),
+                (TextView) v.findViewById(R.id.ingredients),
+                (TextView) v.findViewById(R.id.quantities),
+                (Button) v.findViewById(R.id.go2site) );
+
         return vh;
     }
 
@@ -49,7 +76,32 @@ public class SearchResultsAdapter extends RecyclerView.Adapter<SearchResultsAdap
     public void onBindViewHolder(@NonNull RecipeViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText( recipes.get(position).toString() );
+        final Recipe rec = recipes.get(position);
+        String ingredients = "";
+        String quantities ="";
+        for (Map.Entry e : rec.getIngredient_quantity().entrySet()){
+            ingredients += e.getKey()+"\n";
+            quantities += e.getValue()+"\n";
+        }
+
+        holder.title.setText(rec.getTitle());
+        Integer kc= rec.getCalories();
+        String kcString="";
+        if(kc == null){
+            kcString="Non Disponibile";
+        }else{
+            kcString = kc.toString();
+        }
+        holder.cal.setText("Kcal:\t"+kcString);
+        holder.ingredients.setText(ingredients);
+        holder.quantities.setText( quantities );
+        holder.href.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(rec.getHref()));
+                ContextCompat.startActivity(v.getContext(), browserIntent,null);
+            }
+        });
     }
 
     @Override
