ebed4c715ee07c5aec8d50f450b94e3308d73bce
MatteoRizza <rizzamatteo1996@gmail.com>
2020-05-27 20:09:38 +0200

    ricerca supermercati non funzionante

--- AndroidProj/app/src/main/java/com/example/recipeSE/ShowMarkets.java
+++ AndroidProj/app/src/main/java/com/example/recipeSE/ShowMarkets.java
@@ -1,46 +1,45 @@
 package com.example.recipeSE;
 
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
-import android.content.Context;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentActivity;
 import com.example.recipeSE.login.MainActivity;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.appbar.MaterialToolbar;
 
-import java.util.List;
-
-import static com.facebook.internal.FeatureManager.Feature.Places;
+import java.util.Arrays;
 
 public class ShowMarkets extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private FusedLocationProviderClient mFusedLocationProviderClient;
-    private MaterialToolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +47,10 @@ public class ShowMarkets extends FragmentActivity implements OnMapReadyCallback 
         setContentView(R.layout.activity_show_markets);
 
         //configure toolbar
-        mToolbar = (MaterialToolbar) findViewById(R.id.topAppBar);
+        MaterialToolbar mToolbar = findViewById(R.id.topAppBar);
         mToolbar.setTitle(Html.fromHtml("<font color='#ffffff'>Tuna</font>"));
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @SuppressLint("RtlHardcoded")
             @Override
             public void onClick(View v) {
                 DrawerLayout drawer = findViewById(R.id.drawernavbar);
@@ -60,10 +60,12 @@ public class ShowMarkets extends FragmentActivity implements OnMapReadyCallback 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        assert mapFragment != null;
         mapFragment.getMapAsync(this);
 
 
         mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        Places.initialize(getApplicationContext(), "AIzaSyC7uc8dUyn5YQUeLfDvjqpEUXJJAUMqzf4");
     }
 
 
@@ -96,32 +98,66 @@ public class ShowMarkets extends FragmentActivity implements OnMapReadyCallback 
                     @Override
                     public void onSuccess(Location location) {
                         if (location != null) {
-                            Double lat = location.getLatitude();
-                            Double lon = location.getLongitude();
+                            double lat = location.getLatitude();
+                            double lon = location.getLongitude();
                             LatLng me = new LatLng(lat, lon);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(me));
-                            mMap.moveCamera(CameraUpdateFactory.zoomTo(15));
+                            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(me, 17);
+                            mMap.addMarker(new MarkerOptions().position(me).title("Casa di Matteo"));
+                            mMap.animateCamera(cameraUpdate);
+
+
+                            // Initialize the AutocompleteSupportFragment.
+                            AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+                            // Specify the types of place data to return.
+                            autocompleteFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME, Place.Field.LAT_LNG));
+                            Log.i("TAG", "autocomplete tipo di place specificato");
+                            autocompleteFragment.setText("supermercato");
+                            // Set up a PlaceSelectionListener to handle the response.
+                            autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+                                @Override
+                                public void onPlaceSelected(Place place) {
+                                    // TODO: Get info about the selected place.
+                                    Log.i("Place: ",place.getName() + ", " + place.getId());
+                                    mMap.addMarker(new MarkerOptions().position(place.getLatLng()).title(place.getName()));
+                                }
+
+                                @Override
+                                public void onError(Status status) {
+                                    // TODO: Handle the error.
+                                    Log.i("Errore nel setPlace: ", status.getStatusMessage());
+                                }
+                            });
+
+
 
                         }
                     }
+
+
                 });
+
+        mFusedLocationProviderClient.getLastLocation().addOnFailureListener(new OnFailureListener() {
+            @SuppressLint("LongLogTag")
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                // TODO: Handle the error.
+                Log.i("Errore nel getLastLocation: ", e.getMessage());
+            }
+        });
 
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-        switch (requestCode) {
-            case 200: {
-                if(grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Intent intent = new Intent(getApplicationContext(), ShowMarkets.class);
-                    startActivity(intent);
-                }
-                else{
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                }
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 200) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent intent = new Intent(getApplicationContext(), ShowMarkets.class);
+                startActivity(intent);
+            } else {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         }
     }
-    
+
 }
