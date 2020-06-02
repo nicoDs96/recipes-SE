d02b61ab90a32652cb3f46cee638f6ebb0603b95
nicoDs96 <disanto.1853048@studenti.uniroma1.it>
2020-06-02 17:06:33 +0200

    fixed port crash on heroku

--- app/app.js
+++ app/app.js
@@ -129,7 +129,8 @@ app.use(function (req, res, next) {
     res.status(404).send('Sorry cant find that!');
 });
 
-app.listen(3000, function () {
+//use heroku port remotely while 3000 on local tests
+app.listen(process.env.PORT || 3000, function () {
     console.log('Example app listening on port 3000!');
 });
 
